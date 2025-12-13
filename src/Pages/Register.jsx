7b3d43d264715ddddce0bee6ca9584528c9adcf9
khaled-40 <khaledmasud.dev@gmail.com>
2025-12-14 02:46:19 +0600

    implemented google login for both the register and login page

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,11 +1,12 @@
 import React, { use } from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import { AuthContext } from '../Context/AuthContext';
 import Swal from 'sweetalert2';
 import { updateProfile } from 'firebase/auth';
 
 const Register = () => {
-    const { createUser } = use(AuthContext)
+    const { createUser, signInWithGoogle } = use(AuthContext);
+    const navigate = useNavigate();
     const handleRegisterWithEmailPassword = (e) => {
         e.preventDefault();
         const name = e.target.name.value;
@@ -63,6 +64,44 @@ const Register = () => {
                             showConfirmButton: false,
                             timer: 1500
                         });
+                        navigate('/')
+                    })
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+
+    }
+    const handleSignInWithGoogle = () => {
+        signInWithGoogle()
+            .then(result => {
+                console.log(result.user)
+                const name = result.user.displayName;
+                const email = result.user.email;
+                const photoURL = result.user.photoURL;
+                const newUser = {
+                    name,
+                    email,
+                    photoURL
+                }
+                fetch('https://eco-ngc-bd-server.vercel.app/users', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(newUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log('after new Sign In', data)
+                        Swal.fire({
+                            position: "center",
+                            icon: "success",
+                            title: "Successfully created an user",
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                        navigate('/')
                     })
             })
             .catch(error => {
@@ -92,7 +131,7 @@ const Register = () => {
                     </fieldset>
                 </form>
                 {/* Google */}
-                <button type="button" className="btn w-full mt-3 bg-white text-black border-[#e5e5e5]">
+                <button onClick={handleSignInWithGoogle} type="button" className="btn w-full mt-3 bg-white text-black border-[#e5e5e5]">
                     <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                     Login with Google
                 </button>
